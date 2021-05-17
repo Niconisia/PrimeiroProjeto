a135840e0df8cf0497348ba80d298214ef453177
NicoNicoNii <84341703+Niconisia@users.noreply.github.com>
2021-05-17 14:55:39 -0300

    Update Principal.java

--- src/main/java/com/mycompany/primeiroprojeto/Principal.java
+++ src/main/java/com/mycompany/primeiroprojeto/Principal.java
@@ -13,6 +13,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
         System.out.println("Olá Nicolas!");
+        //Testes e mais Testes AAAAAAAAAAAAAAAAAA
     }
     
 }
